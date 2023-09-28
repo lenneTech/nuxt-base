37dfcbe3fb0794a7704a849af98139b6f62781da
Pascal Klesse <pascal.klesse@lenne.tech>
2023-09-28 11:17:56 +0200

    fix plugins

--- src/runtime/plugins/01.graphql.ts
+++ src/runtime/plugins/01.graphql.ts
@@ -2,23 +2,27 @@ import { callWithNuxt, defineNuxtPlugin, useNuxtApp, useRuntimeConfig } from 'nu
 import type { GraphQLMeta } from '../classes/graphql-meta.class';
 import { loadMeta } from '../functions/graphql-meta';
 
-export default defineNuxtPlugin(async (_nuxt) => {
-  const nuxt = useNuxtApp();
-  const config = await callWithNuxt(nuxt, useRuntimeConfig);
-  let meta: GraphQLMeta | null = null;
+export default defineNuxtPlugin({
+  name: 'graphql',
+  enforce: 'post',
+  async setup() {
+    const nuxt = useNuxtApp();
+    const config = await callWithNuxt(nuxt, useRuntimeConfig);
+    let meta: GraphQLMeta | null = null;
 
-  console.debug('01.graphql.ts::init');
+    console.debug('01.graphql.ts::init');
 
-  try {
-    meta = await callWithNuxt(nuxt, loadMeta, [{ public: config.public }]);
-  } catch (e) {
-    console.error('$graphql::loadMeta::error - Please check connection to your host.');
-    meta = null;
-  }
+    try {
+      meta = await callWithNuxt(nuxt, loadMeta, [{ public: config.public }]);
+    } catch (e) {
+      console.error('$graphql::loadMeta::error - Please check connection to your host.');
+      meta = null;
+    }
 
-  return {
-    provide: {
-      graphQl: () => meta,
-    },
-  };
+    return {
+      provide: {
+        graphQl: () => meta,
+      },
+    };
+  },
 });
--- src/runtime/plugins/03.apollo.ts
+++ src/runtime/plugins/03.apollo.ts
@@ -1,4 +1,4 @@
-import { callWithNuxt, defineNuxtPlugin } from 'nuxt/app';
+import { callWithNuxt, defineNuxtPlugin, useNuxtApp } from 'nuxt/app';
 import type { ApolloClient } from '@apollo/client/core';
 import { ApolloLink, from, fromPromise } from '@apollo/client/core';
 import { onError } from '@apollo/client/link/error';
@@ -9,87 +9,91 @@ import { useAuth } from '../composables/use-auth';
 /**
  * See example: https://github.com/nuxt-modules/apollo/issues/442
  */
-export default defineNuxtPlugin((nuxtApp) => {
-  console.debug('03.apollo.ts::init');
-  const { $apollo } = nuxtApp;
-  const defaultClient = ($apollo as any).defaultClient as unknown as ApolloClient<any>;
+export default defineNuxtPlugin({
+  name: 'apollo',
+  enforce: 'post',
+  async setup() {
+    console.debug('03.apollo.ts::init');
+    const nuxtApp = useNuxtApp();
+    const defaultClient = (nuxtApp.$apollo as any).defaultClient as unknown as ApolloClient<any>;
 
-  // trigger the error hook on an error
-  const errorLink = onError((err) => {
-    const { requestNewToken, clearSession } = useAuth();
+    // trigger the error hook on an error
+    const errorLink = onError((err) => {
+      const { requestNewToken, clearSession } = useAuth();
 
-    if (err.graphQLErrors) {
-      for (const error of err.graphQLErrors) {
-        switch (error.extensions.code) {
-          case 'UNAUTHENTICATED': {
-            if (
-              error.message !== 'Expired refresh token' &&
+      if (err.graphQLErrors) {
+        for (const error of err.graphQLErrors) {
+          switch (error.extensions.code) {
+            case 'UNAUTHENTICATED': {
+              if (
+                error.message !== 'Expired refresh token' &&
                 error.message !== 'Expired token' &&
                 error.message !== 'Invalid token'
-            ) {
-              return;
-            }
+              ) {
+                return;
+              }
 
-            if (error.message === 'Expired refresh token' || error.message === 'Invalid token') {
-              clearSession();
-              return;
-            }
+              if (error.message === 'Expired refresh token' || error.message === 'Invalid token') {
+                clearSession();
+                return;
+              }
 
-            return fromPromise(callWithNuxt(nuxtApp, requestNewToken) as any)
-              .filter((value) => Boolean(value))
-              .flatMap((response: any) => {
-                console.debug('03.apollo.ts::errorLink::retryRequest', response.token);
-                const oldHeaders = err.operation.getContext().headers;
+              return fromPromise(callWithNuxt(nuxtApp, requestNewToken) as any)
+                .filter((value) => Boolean(value))
+                .flatMap((response: any) => {
+                  console.debug('03.apollo.ts::errorLink::retryRequest', response.token);
+                  const oldHeaders = err.operation.getContext().headers;
 
-                // modify the operation context with a new token
-                err.operation.setContext({
-                  headers: {
-                    ...oldHeaders,
-                    Authorization: `Bearer ${response.token}`,
-                  },
+                  // modify the operation context with a new token
+                  err.operation.setContext({
+                    headers: {
+                      ...oldHeaders,
+                      Authorization: `Bearer ${response.token}`,
+                    },
+                  });
+
+                  // retry the request, returning the new observable
+                  return err.forward(err.operation);
                 });
-
-                // retry the request, returning the new observable
-                return err.forward(err.operation);
-              });
+            }
           }
         }
       }
-    }
-  });
+    });
 
-  const authMiddleware = new ApolloLink((operation, forward) => {
-    const headers: Record<string, string> = {};
-    const operationName = (operation.query.definitions[0] as any)?.selectionSet?.selections[0]?.name?.value;
-    const { accessTokenState, refreshTokenState } = useAuthState();
+    const authMiddleware = new ApolloLink((operation, forward) => {
+      const headers: Record<string, string> = {};
+      const operationName = (operation.query.definitions[0] as any)?.selectionSet?.selections[0]?.name?.value;
+      const { accessTokenState, refreshTokenState } = useAuthState();
 
-    if (accessTokenState.value && refreshTokenState.value) {
-      let token: string;
+      if (accessTokenState.value && refreshTokenState.value) {
+        let token: string;
 
-      if (operationName === 'refreshToken') {
-        token = refreshTokenState.value || null;
-      } else {
-        token = accessTokenState.value || null;
+        if (operationName === 'refreshToken') {
+          token = refreshTokenState.value || null;
+        } else {
+          token = accessTokenState.value || null;
+        }
+
+        if (token) {
+          headers.Authorization = 'Bearer ' + token;
+        }
+
+        operation.setContext(() => ({ headers }));
       }
+      return forward(operation);
+    });
 
-      if (token) {
-        headers.Authorization = 'Bearer ' + token;
-      }
+    // Set custom links in the apollo client.
+    // This is the link chain. Will be walked through from top to bottom. It can only contain 1 terminating
+    // Apollo link, see: https://www.apollographql.com/docs/react/api/link/introduction/#the-terminating-link
+    defaultClient.setLink(from([
+      authMiddleware,
+      errorLink,
+      defaultClient.link,
+    ]));
 
-      operation.setContext(() => ({ headers }));
-    }
-    return forward(operation);
-  });
-
-  // Set custom links in the apollo client.
-  // This is the link chain. Will be walked through from top to bottom. It can only contain 1 terminating
-  // Apollo link, see: https://www.apollographql.com/docs/react/api/link/introduction/#the-terminating-link
-  defaultClient.setLink(from([
-    authMiddleware,
-    errorLink,
-    defaultClient.link,
-  ]));
-
-  // For using useQuery in `@vue/apollo-composable`
-  provideApolloClient(defaultClient);
+    // For using useQuery in `@vue/apollo-composable`
+    provideApolloClient(defaultClient);
+  },
 });
