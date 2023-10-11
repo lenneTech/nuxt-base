48caf445e83b37099f479c617786728796f87802
Pascal Klesse <pascal.klesse@lenne.tech>
2023-10-11 09:13:12 +0200

    fix: Change return type of query

--- src/generate.ts
+++ src/generate.ts
@@ -80,9 +80,8 @@ export async function generateComposables(meta: GraphQLMeta): Promise<string> {
   template.push(
     'import { gqlQuery, gqlMutation, gqlSubscription } from \'#imports\';\n',
   );
-  template.push('import type { AsyncData } from \'nuxt/dist/app/composables\';\n');
   template.push(
-    'import { UseMutationReturn, UseSubscriptionReturn } from \'@vue/apollo-composable\';\n',
+    'import { UseMutationReturn, UseSubscriptionReturn, UseQueryReturn } from \'@vue/apollo-composable\';\n',
   );
 
   if (methods?.query) {
@@ -94,7 +93,7 @@ export async function generateComposables(meta: GraphQLMeta): Promise<string> {
       template.push(
         `export const use${capitalizeFirstLetter(query)}Query = (${
           types.argType ? 'variables: { ' + types.argType + ' },' : ''
-        } fields?: InputFields<${inputFieldsType}>[] | null, log?: boolean): Promise<AsyncData<{${query}: ${
+        } fields?: InputFields<${inputFieldsType}>[] | null, log?: boolean): Promise<UseQueryReturn<{${query}: ${
           types.returnType
         }}, any>> => gqlQuery<{${query}: ${types.returnType}}>('${query}', {${
           types.argType ? 'variables,' : ''
