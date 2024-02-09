4cf1322f6d2cb87f5e5d453bbc52d5563bd81163
Pascal Klesse <pascal.klesse@lenne.tech>
2024-02-09 18:23:59 +0100

    fix: fix array handling in parseVariables

--- src/runtime/classes/graphql-meta.class.ts
+++ src/runtime/classes/graphql-meta.class.ts
@@ -90,7 +90,15 @@ export class GraphQLMeta {
 
     if (typeof variables === 'object' && Object.keys(fields)?.length) {
       for (const [key, value] of Object.entries(variables)) {
-        console.log('key', key, 'value', value, 'fields', fields[key]);
+        if (log) {
+          console.debug('key', key, 'value', value, 'fields', fields[key]);
+        }
+
+        if (Array.isArray(value)) {
+          result[key] = value.map((item) => this.parseVariables(item, fields[key].fields, log));
+          continue;
+        }
+
         switch (fields[key].type) {
           case 'String':
             result[key] = value;
