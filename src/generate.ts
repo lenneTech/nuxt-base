0de5029b15966abc1f4b45b2d40fc31ba7f63dd4
Pascal Klesse <pascal.klesse@lenne.tech>
2024-05-21 23:15:56 +0200

    feat: update packages

--- src/generate.ts
+++ src/generate.ts
@@ -1,27 +1,27 @@
-import { generate } from '@graphql-codegen/cli';
 import type { Types } from '@graphql-codegen/plugin-helpers';
+import type { Import } from 'unimport';
+
+import { generate } from '@graphql-codegen/cli';
+import { addTemplate } from '@nuxt/kit';
 import { buildClientSchema, getIntrospectionQuery } from 'graphql';
 import { ofetch } from 'ofetch';
-import type { Import } from 'unimport';
+
 import { GraphQLMeta } from './runtime/classes/graphql-meta.class';
-import { addTemplate } from '@nuxt/kit';
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 const { loadSchema } = require('@graphql-tools/load');
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
 
-export async function loadMetaServer(
-  config: Partial<{ public: { host: string; schema?: string } }>,
-): Promise<GraphQLMeta> {
+export async function loadMetaServer(config: Partial<{ public: { host: string; schema?: string } }>): Promise<GraphQLMeta> {
   let schema;
 
   if (!config.public.schema) {
     const { data: result } = await ofetch(config.public.host, {
-      method: 'POST',
       body: JSON.stringify({
         query: getIntrospectionQuery({ descriptions: false }),
         variables: {},
       }),
+      method: 'POST',
     });
 
     schema = buildClientSchema(result);
@@ -36,35 +36,35 @@ export async function loadMetaServer(
 
 export default async function generateGraphQLTypes(schema: string) {
   const config: Types.Config = {
-    schema,
-    ignoreNoDocuments: true,
     config: {
-      skipTypename: true,
+      declarationKind: 'interface',
+      maybeValue: 'T',
       namingConvention: {
         enumValues: 'change-case-all#upperCase',
       },
-      maybeValue: 'T',
-      useTypeImports: true,
-      declarationKind: 'interface',
-      scalars: {},
       primitives: {
-        ID: 'string',
         Any: 'any',
-        String: 'string',
-        Float: 'number',
-        Int: 'number',
         Boolean: 'boolean',
-        DateTime: 'Date',
         Date: 'Date',
+        DateTime: 'Date',
+        Float: 'number',
+        ID: 'string',
+        Int: 'number',
         JSON: '{ [key: string]: any }',
+        String: 'string',
         Upload: 'File',
       },
+      scalars: {},
+      skipTypename: true,
+      useTypeImports: true,
     },
     generates: {
       [process.cwd() + '/src/types/schema.d.ts']: {
         plugins: ['typescript'],
       },
     },
+    ignoreNoDocuments: true,
+    schema,
   };
 
   return await generate(config, false);
@@ -74,18 +74,10 @@ export async function generateComposables(meta: GraphQLMeta): Promise<string> {
   const methods = meta.getMethodNames();
   const template = [];
   let customTypes = [];
-  template.push(
-    'import type { InputFields } from \'#base-types/fields\';\n',
-  );
-  template.push(
-    'import { gqlQuery, gqlMutation, gqlSubscription } from \'#imports\';\n',
-  );
-  template.push(
-    'import { UseMutationReturn, UseSubscriptionReturn } from \'@vue/apollo-composable\';\n',
-  );
-  template.push(
-    'import type { AsyncData } from \'nuxt/app\';',
-  );
+  template.push('import type { InputFields } from \'#base-types/fields\';\n');
+  template.push('import { gqlQuery, gqlMutation, gqlSubscription } from \'#imports\';\n');
+  template.push('import { UseMutationReturn, UseSubscriptionReturn } from \'@vue/apollo-composable\';\n');
+  template.push('import type { AsyncData } from \'nuxt/app\';');
 
   if (methods?.query) {
     for (const query of methods.query) {
@@ -98,9 +90,7 @@ export async function generateComposables(meta: GraphQLMeta): Promise<string> {
           types.argType ? 'variables: { ' + types.argType + ' },' : ''
         } fields?: InputFields<${inputFieldsType}>[] | null, lazy?: boolean, log?: boolean): Promise<AsyncData<{${query}: ${
           types.returnType
-        }}, any>> => gqlQuery<{${query}: ${types.returnType}}>('${query}', {${
-          types.argType ? 'variables,' : ''
-        } fields, lazy, log})`,
+        }}, any>> => gqlQuery<{${query}: ${types.returnType}}>('${query}', {${types.argType ? 'variables,' : ''} fields, lazy, log})`,
       );
     }
   }
@@ -113,13 +103,9 @@ export async function generateComposables(meta: GraphQLMeta): Promise<string> {
       template.push(
         `export const use${capitalizeFirstLetter(mutation)}Mutation = (${
           types.argType ? 'variables: { ' + types.argType + ' },' : ''
-        } fields?: InputFields<${inputFieldsType}>[] | null, log?: boolean): Promise<UseMutationReturn<{${mutation}: ${
+        } fields?: InputFields<${inputFieldsType}>[] | null, log?: boolean): Promise<UseMutationReturn<{${mutation}: ${types.returnType}}, any>> => gqlMutation<{${mutation}: ${
           types.returnType
-        }}, any>> => gqlMutation<{${mutation}: ${
-          types.returnType
-        }}>('${mutation}', {${
-          types.argType ? 'variables,' : ''
-        } fields, log})`,
+        }}>('${mutation}', {${types.argType ? 'variables,' : ''} fields, log})`,
       );
     }
   }
@@ -131,17 +117,11 @@ export async function generateComposables(meta: GraphQLMeta): Promise<string> {
       const inputFieldsType = types.returnType.replace('[]', '');
 
       template.push(
-        `export const use${capitalizeFirstLetter(
-          subscription,
-        )}Subscription = (${
+        `export const use${capitalizeFirstLetter(subscription)}Subscription = (${
           types.argType ? 'variables: { ' + types.argType + ' },' : ''
         } fields?: InputFields<${inputFieldsType}>[] | null, log?: boolean): Promise<UseSubscriptionReturn<{${subscription}: ${
           types.returnType
-        }}, any>> => gqlSubscription<{${subscription}: ${
-          types.returnType
-        }}>('${subscription}', {${
-          types.argType ? 'variables,' : ''
-        } fields, log})`,
+        }}, any>> => gqlSubscription<{${subscription}: ${types.returnType}}>('${subscription}', {${types.argType ? 'variables,' : ''} fields, log})`,
       );
     }
   }
@@ -180,15 +160,9 @@ export async function getAllImports(meta: GraphQLMeta) {
   customTypes = [...new Set([].concat(...customTypes))];
 
   return [
-    ...customTypes.map(
-      (type): Import => ({ from: '#base/default', name: type }),
-    ),
-    ...methods.query.map(
-      (fn): Import => ({ from: '#base', name: getMethodName(fn, 'Query') }),
-    ),
-    ...methods.mutation.map(
-      (fn): Import => ({ from: '#base', name: getMethodName(fn, 'Mutation') }),
-    ),
+    ...customTypes.map((type): Import => ({ from: '#base/default', name: type })),
+    ...methods.query.map((fn): Import => ({ from: '#base', name: getMethodName(fn, 'Query') })),
+    ...methods.mutation.map((fn): Import => ({ from: '#base', name: getMethodName(fn, 'Mutation') })),
     ...methods.subscription.map(
       (fn): Import => ({
         from: '#base',
@@ -211,22 +185,20 @@ export async function generateFiles(options: any, logger: any, nuxt: any, resolv
     const meta = await loadMetaServer({ public: options });
 
     // Generate graphql types
-    const generatedTypes = await generateGraphQLTypes(
-      options.schema ?? options.host,
-    );
+    const generatedTypes = await generateGraphQLTypes(options.schema ?? options.host);
     addTemplate({
-      write: true,
       filename: nuxt.options.rootDir + '/src/base/default.ts',
       getContents: () => generatedTypes[0].content || '',
+      write: true,
     });
     logger.success('[@lenne.tech/nuxt-base] Generated base/default.ts');
 
     // Generate composable types
     const composables = await generateComposables(meta);
     addTemplate({
-      write: true,
       filename: nuxt.options.rootDir + '/src/base/index.ts',
       getContents: () => composables || '',
+      write: true,
     });
     logger.success('[@lenne.tech/nuxt-base] Generated base/index.ts');
 
@@ -237,23 +209,12 @@ export async function generateFiles(options: any, logger: any, nuxt: any, resolv
         imports.push(...(methods || []));
       });
 
-      nuxt.options.alias['#base'] = resolver.resolve(
-        nuxt.options.rootDir,
-        'src',
-        'base',
-      );
+      nuxt.options.alias['#base'] = resolver.resolve(nuxt.options.rootDir, 'src', 'base');
 
-      nuxt.options.alias['#base/*'] = resolver.resolve(
-        nuxt.options.rootDir,
-        'src',
-        'base',
-        '*',
-      );
+      nuxt.options.alias['#base/*'] = resolver.resolve(nuxt.options.rootDir, 'src', 'base', '*');
     }
   } catch (e) {
     console.error(e);
-    logger.warn(
-      '[@lenne.tech/nuxt-base] Generated failed. Please check your host.',
-    );
+    logger.warn('[@lenne.tech/nuxt-base] Generated failed. Please check your host.');
   }
 }
